9ae6e3688818da57d25e7035b22a5bc4b42d371d
Riddzx <ridha8566@gmail.com>
2023-06-06 15:15:17 +0700

    make a toggle switch

--- js/script.js
+++ js/script.js
@@ -191,6 +191,7 @@ shoppingCartIcon.appendChild(notificationBadge);
 
 const toggleButton = document.querySelector("#toggle-mode");
 const themeContainer = document.querySelector(".theme-container");
+const iconMode = document.querySelector("#icon-mode");
 
 document.querySelector("#toggle-mode").onclick = (e) => {
   themeContainer.classList.toggle("light-mode");
@@ -198,9 +199,11 @@ document.querySelector("#toggle-mode").onclick = (e) => {
 
   //icon swith mode
   if (themeContainer.classList.contains("light-mode")) {
-    toggleButton.innerHTML = '<i data-feather="sun"></i>';
+    toggleButton.innerHTML = '<i data-feather="toggle-right"></i>';
+    iconMode.innerHTML = '<i data-feather="sun"></i>';
   } else {
-    toggleButton.innerHTML = '<i data-feather="moon"></i>';
+    toggleButton.innerHTML = '<i data-feather="toggle-left"></i>';
+    iconMode.innerHTML = '<i data-feather="moon"></i>';
   }
 
   feather.replace();
